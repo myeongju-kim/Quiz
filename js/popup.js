eda187a1007691e60ed14aa532c556f148f4731c
김명주 <kingmj@daou.co.kr>
2023-10-12 19:02:13 +0900

    create loading bar

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,18 @@
-function showPopup() {
+function deletePopup() {
     alert('오답입니다.');
+}
+
+function correctPopup(page) {
+    const correct = document.getElementById('correct');
+    const loadingBar = document.getElementById('loading');
+    // 초기 너비를 100%로 설정
+
+    correct.style.display = 'block';
+    setTimeout(function () {
+        loadingBar.style.width = '100%';
+    }, 50);
+    // 3초 뒤에 페이지 이동
+    setTimeout(function () {
+        window.location.href = page;
+    }, 3000);
 }
